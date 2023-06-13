766a80168963af0e0726c4bb30a6602e64e74224
Dash <96434604+HumanThe2nd@users.noreply.github.com>
2023-06-13 09:31:40 -0400

    Update and rename SolutionTesting.py to aac3p1.py

--- aac3p1.py
+++ aac3p1.py
@@ -1,9 +1,3 @@
-# n = int(input());
-# for i in range(n):
-#     inp = input();
-#     start = 2;
-#     if inp[1] != " ":
-#         start = 3;
 n = input().split(" ")
 if int(n[0]) < int(n[1]):
      if int(n[2]) < int(n[3]):
