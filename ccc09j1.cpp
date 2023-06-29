d9b958d4cdad7fbd9a9352364ab40ff43e03365f
Dash <96434604+HumanThe2nd@users.noreply.github.com>
2023-06-29 09:41:43 -0400

    Create ccc09j1.cpp

---
+++ ccc09j1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int num = 0;
+  string inp;
+  int j;
+  string ISBN = "9780921418";
+  for(int i = 0; i < 3;i++){
+    cin >> inp;
+    ISBN += inp;
+  }
+  for(int i = 0;i < 13;i++){
+    if(i%2 ==0) j=1;
+    else j = 3;
+    num += (j)*(ISBN[i]-48);
+  }
+  cout << "The 1-3-sum is "<< num;
+}
