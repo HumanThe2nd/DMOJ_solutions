3c37c584a70e1ae766b7cde3afa5fadae66ef90a
Dash <96434604+HumanThe2nd@users.noreply.github.com>
2023-06-30 14:52:57 -0400

    Create ccc00j1.py

---
+++ ccc00j1.py
@@ -0,0 +1,18 @@
+inp = input().split();
+start = int(inp[0]);
+days = int(inp[1]);
+print("Sun Mon Tue Wed Thr Fri Sat");
+print("    "*(start-1),end =" ")
+for i in range(1,days+1):
+    if(i<10):
+        print(f" {i}",end = "")
+    else:
+        print(i,end = "")
+    if(i == days):
+      break;
+    elif(i+start-1)%7 ==0:
+        print()
+        print(" ",end="")
+    else:
+        print("  ",end="")
+print();
