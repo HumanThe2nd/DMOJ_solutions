342ebce2c568a27cb2161f3a532671c77b4dd973
Dash <96434604+HumanThe2nd@users.noreply.github.com>
2023-07-08 12:50:24 -0400

    Create ccc19j2.cpp

---
+++ ccc19j2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+using namespace std;
+using ll = long long;
+int main() {
+  int n,n2;
+  string inp;
+  cin >> n;
+  for(int i = 0; i < n;i++){
+    cin >> n2 >> inp;
+    for(int j = 0; j< n2;j++){
+      cout << inp;
+    }
+    cout << "\n";
+  }
+}
