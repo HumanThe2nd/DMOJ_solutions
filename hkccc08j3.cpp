2df86fac837487631b2de916f015e12b667c6f27
Dash <96434604+HumanThe2nd@users.noreply.github.com>
2023-07-08 18:38:59 -0400

    Create hkccc08j3.cpp

---
+++ hkccc08j3.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+using namespace std;
+using ll = long long;
+int main() {
+  int n,n2,num,max = 0,index = 0;
+  string name;
+  vector<string> names;
+  vector<int> numbers,count;
+  cin >> n;
+  for(int i = 0;i<n;i++){
+    cin >> name >> num;
+    names.push_back(name);
+    numbers.push_back(num);
+    count.push_back(0);
+  }
+  cin >> n2;
+  for(int i = 0;i < n2;i++){
+    cin >> num;
+    for(int j = 0; j < n;j++){
+      if(num == numbers[j]) count[j]++;
+    }
+  }
+  for(int i = 0; i< n;i++){
+    if(count[i] > max || (count[i] == max && numbers[i] < numbers[index])){
+      index = i;
+      max = count[i];
+    }
+  }
+  cout << names[index];
+}
