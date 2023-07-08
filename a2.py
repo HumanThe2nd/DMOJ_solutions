979d3f63a1e2884b5bb1efd1ba41fa1b0202f3d8
Dash <96434604+HumanThe2nd@users.noreply.github.com>
2023-07-08 12:57:58 -0400

    Create a2.py

---
+++ a2.py
@@ -0,0 +1,18 @@
+// a2 Mirrored Pairs
+// by Dan Shan
+run = True;
+print("Ready")
+while run == True:
+    inp = input()
+    if inp[0] == "q" and inp[1] == "p":
+        print("Mirrored pair")
+    elif inp[0] == "p" and inp[1] == "q":
+        print("Mirrored pair")
+    elif inp[0] == "b" and inp[1] == "d":
+        print("Mirrored pair")
+    elif inp[0] == "d" and inp[1] == "b":
+        print("Mirrored pair")
+    elif inp == "  ":
+        break;
+    else:
+        print("Ordinary pair")
