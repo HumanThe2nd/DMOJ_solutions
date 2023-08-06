f9a9b7e0e724e3cea65de7f0002849447c1dc2b8
Dash <96434604+HumanThe2nd@users.noreply.github.com>
2023-08-06 17:28:41 -0400

    Update oly20practice75.cpp

--- oly20practice75.cpp
+++ oly20practice75.cpp
@@ -4,21 +4,21 @@
 using namespace std;
 using ll = long long;
 int main() {
-  int I,N,J,c=0;
-  scanf("%d %d %d",&I,&N,&J);
+  int n,m,c=0;
+  scanf("%d %d",&n,&m);
   vector<int> diff,psa;
-  for(int i=0;i<I;i++) diff.push_back(0);
-  for(int i=0;i<J;i++){
-    int xi,xf,k;
-    scanf("%d %d %d",&xi,&xf,&k);
-    xi--;
-    diff[xi]+=k;
-    diff[xf]-=k;
+  for(int i=0;i<n;i++) diff.push_back(0);
+  for(int i=0;i<m;i++){
+    int s,e;
+    scanf("%d %d",&s,&e);
+    s--;
+    diff[s]++;
+    diff[e]--;
   }
   psa.push_back(diff[0]);
-  for(int i=1;i<I;i++)psa.push_back(psa[i-1]+diff[i]);
+  for(int i=1;i<n;i++)psa.push_back(psa[i-1]+diff[i]);
   for(auto x:psa){
-    if(x<N)c++;
+    if(x%2==1)c++;
   }
   printf("%d %s",c,"\n");
 }
