4dde7a6f806ef71cbf312d9a0189ce0779a7686e
Dash <96434604+HumanThe2nd@users.noreply.github.com>
2023-08-06 17:29:25 -0400

    Create seed3.cpp

---
+++ seed3.cpp
@@ -0,0 +1,24 @@
+// The Cosmic Era P3 - Battle Positions
+// By: Dan Shan
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+int main() {
+  int I,N,J,c=0;
+  scanf("%d %d %d",&I,&N,&J);
+  vector<int> diff,psa;
+  for(int i=0;i<I;i++) diff.push_back(0);
+  for(int i=0;i<J;i++){
+    int xi,xf,k;
+    scanf("%d %d %d",&xi,&xf,&k);
+    xi--;
+    diff[xi]+=k;
+    diff[xf]-=k;
+  }
+  psa.push_back(diff[0]);
+  for(int i=1;i<I;i++)psa.push_back(psa[i-1]+diff[i]);
+  for(auto x:psa){
+    if(x<N)c++;
+  }
+  printf("%d %s",c,"\n");
+}
