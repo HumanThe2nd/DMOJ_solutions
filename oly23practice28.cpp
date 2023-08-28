bec63808490c147a04766150af724dce3d1236b1
Dan Shan <96434604+HumanThe2nd@users.noreply.github.com>
2023-08-27 22:43:28 -0400

    Create oly23practice28.cpp

---
+++ oly23practice28.cpp
@@ -0,0 +1,18 @@
+Distinct Array
+// By: Dan Shan
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  unordered_set<int> s;
+  int n;
+  scanf("%d",&n);
+  bool d=1;
+  for(int i=0;i<n;i++){
+    int in;
+    scanf("%d",&in);
+    if(s.find(in)!=s.end()) {d=0; break;}
+    else s.insert(in);
+  }
+  if(d) printf("YES");
+  else printf("NO");
+}
