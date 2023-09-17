f00de527905d2f14324618e00e65f337d17cf1ae
Dan Shan <96434604+HumanThe2nd@users.noreply.github.com>
2023-09-17 12:42:13 -0400

    Create ccc23s2.cpp

---
+++ ccc23s2.cpp
@@ -0,0 +1,40 @@
+//============================================================================
+// Name        : CCC '23 S2 - Symmetric Mountains
+// Author      : Dan Shan
+//============================================================================
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int n;
+	vector<int> m,res;
+	scanf("%d",&n);
+	for(int i=0;i<n;i++){
+		int in;
+		scanf("%d",&in);
+		m.push_back(in);
+		res.push_back(1000000);
+	}
+	res[0]=0;
+	for(int i=0;i<n-1;i++){
+		int r=i+1,l=i,a=0;
+		while(l>=0&&r<n){
+			int s=r-l;
+			a+=abs(m[l]-m[r]);
+			res[s]=min(res[s],a);
+			r++; l--;
+		}
+	}
+	for(int i=1;i<n-1;i++){
+		int l=i-1,r=i+1,a=0;
+		while(l>=0&&r<n){
+			int s = r-l;
+			a+=abs(m[l]-m[r]);
+			res[s]=min(res[s],a);
+			r++; l--;
+		}
+	}
+	for(int i=0;i<n-1;i++)printf("%d ",res[i]);
+	printf("%d\n",res.back());
+}
