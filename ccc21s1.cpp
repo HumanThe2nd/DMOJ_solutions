c0d965abe35453102355f547036cda97437a93e6
Dan Shan <96434604+HumanThe2nd@users.noreply.github.com>
2023-10-01 12:25:33 -0400

    Create ccc21s1.cpp

---
+++ ccc21s1.cpp
@@ -0,0 +1,27 @@
+//============================================================================
+// Name        : CCC '21 S1 - Crazy Fencing
+// Author      : Dan Shan
+//============================================================================
+// Goal: find area of a chain of quadrilaterals
+// Approach: input heights then sum the area between each 2 fences
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+int main() {
+int n;
+double c;
+scanf("%d",&n);
+vector<int> heights;
+for(int i=0;i<=n;i++){
+	int in;
+	scanf("%d",&in);
+	heights.push_back(in);
+}
+for(int i=0;i<n;i++){
+	int in;
+	scanf("%d",&in);
+	c+=in*((double)heights[i]+heights[i+1])/2;
+}
+printf("%lf\n",c);
+return 0;
+}
