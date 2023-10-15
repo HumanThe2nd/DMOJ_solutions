6c51a55259c2bea5b6f6c4caf2153ef9126bc37a
Dan Shan <96434604+HumanThe2nd@users.noreply.github.com>
2023-10-15 12:30:31 -0400

    Update and rename ccc19s2.cpp to ccc19s2.c

--- ccc19s2.c
+++ ccc19s2.c
@@ -1,11 +1,11 @@
 // CCC '19 S2 - Pretty Average Primes
 // By: Dan Shan
-#include <bits/stdc++.h>
-using namespace std;
-bool check(double n){
-  bool prime = 1;
+#include <stdio.h> 
+#include <math.h>
+int check(double n){
+  int prime = 1;
 for(int i=2;i<pow(n,0.5)+1;i++){
-    if(int(n/i)==double(n/i)){
+    if((int)(n/i)==(double)(n/i)){
       prime = 0;
       break;
   }
@@ -19,7 +19,7 @@ int main(){
     double in;
     int out,p=0;
     scanf("%lf",&in);
-      while(check(in+p)==false||check(in-p)==false) p++;
+      while(check(in+p)==0||check(in-p)==0) p++;
       out = in;
       printf("%d %d %s",out-p,out+p,"\n");
   }
