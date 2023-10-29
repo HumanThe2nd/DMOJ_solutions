62016bc59f3501d4e6dc10a9436e55144b4f6f58
Dan Shan <96434604+HumanThe2nd@users.noreply.github.com>
2023-10-28 22:51:51 -0400

    Create a20.c

---
+++ a20.c
@@ -0,0 +1,21 @@
+// A20 Gate
+#include <stdio.h>
+#include <string.h>
+int main() {
+    char t[] = "0123456789ABCDEF";
+    int c;
+    scanf("%d",&c);
+    for(int i=0;i<c;i++) {
+        char n[9];
+        scanf("%s", n);
+        int ind = strchr(t, n[2]) - t;
+        if(ind % 2 == 0) {
+            printf("%s\n", n);
+        } else {
+            char n2[100];
+            strcpy(n2, n);
+            n2[2] = t[ind - 1];
+            printf("%s %s\n", n2, n);
+        }
+    }
+}
