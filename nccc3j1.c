bd43443e7a1b9d15b352bc46e2d27e9f502b4870
Dan Shan <96434604+HumanThe2nd@users.noreply.github.com>
2023-11-30 19:04:38 -0500

    Create nccc3j1.c

---
+++ nccc3j1.c
@@ -0,0 +1,11 @@
+// Mock CCC '18 Contest 3 J1 - A Sum Problem
+// By: Dan Shan
+#include <stdio.h>
+int n,c,a;
+int main() {
+    scanf("%d",&n);
+    for(int i=0;i<n;i++){
+        scanf("%d",&a); c+=a;
+    }
+    printf("%d\n",c);
+}
