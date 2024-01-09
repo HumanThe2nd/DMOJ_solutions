02f109a296e1c7a68fb030c73c15b832f23ab391
Dan Shan <96434604+HumanThe2nd@users.noreply.github.com>
2024-01-09 08:54:51 -0500

    Create hailstone.c

---
+++ hailstone.c
@@ -0,0 +1,16 @@
+// HailStone Numbers
+// By: Dan Shan
+#include<stdio.h>
+int main(){
+int n,c=0;
+scanf("%d",&n);
+while(n>1){
+	c++;
+	if(n&1){
+		n*=3;
+		n++;
+	}
+	else n>>=1;
+}
+printf("%d\n",c);
+}
