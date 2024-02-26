c8705eac766bbe5e677a49b85da24b6e7203351d
Dan Shan <96434604+HumanThe2nd@users.noreply.github.com>
2024-02-26 09:01:35 -0500

    Create othscc1p1.c

---
+++ othscc1p1.c
@@ -0,0 +1,9 @@
+// OTHS Coding Competition 1 (Mock CCC) J1 - Math Assistance
+// By: Dan Shan
+// My first published problem
+#include <stdio.h>
+int main() {
+    short int a,b;
+    scanf("%hd %hd",&a,&b);
+    printf("%d\n",!a||!b?0:a+b);
+}
