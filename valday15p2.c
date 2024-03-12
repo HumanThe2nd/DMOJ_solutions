163d4ccd76cb09785b925f3f5c28f4b4ba974a80
Dan Shan <96434604+HumanThe2nd@users.noreply.github.com>
2024-03-12 01:39:22 -0400

    Update valday15p2.c

--- valday15p2.c
+++ valday15p2.c
@@ -1,27 +1,21 @@
 // Valentine's Day '15 P2 - Simon and Marcy
 // By: Dan Shan
-// template knapsack dp implementation
-#include<stdio.h>
-int max(int a,int b){return(a>b)?a:b;}
+// bounded knapsack
+#include <stdio.h>
+#define max(a,b)((a)>(b)?(a):(b))
 int main() {
-	int c,m; // m: max weight
-	scanf("%d %d",&c,&m);
-    int val[c], wt[c];
-    for(int i=0;i<c;i++){
-    	scanf("%d %d",&val[i],&wt[i]);
+    int c,m;
+    scanf("%d %d",&c,&m);
+    int v[c],w[c],dp[m+1];
+    for(int i=0;i<c;i++) {
+        scanf("%d %d",&v[i],&w[i]);
     }
-    int n = sizeof(val)/sizeof(val[0]);
-    int i, w;
-    int K[n+1][m+1];
-    for (i = 0; i <= n; i++) {
-        for (w = 0; w <= m; w++) {
-            if (i == 0 || w == 0)
-                K[i][w] = 0;
-            else if (wt[i-1] <= w)
-                  K[i][w] = max(val[i-1] + K[i-1][w-wt[i-1]],  K[i-1][w]);
-            else
-                  K[i][w] = K[i-1][w];
+    for(int i=0;i<=m;i++) dp[i]=0;
+    for(int i=1;i<=c;i++){
+        for(int j=m;j>=0;j--){
+            if(w[i-1]>j) continue;
+            dp[j]=max(dp[j],dp[j-w[i-1]]+v[i-1]);
         }
     }
-    printf("%d\n", K[n][m]);
+    printf("%d\n",dp[m]);
 }
