9a36c523efe5384315c23d7289432ffb872994be
Dan Shan <96434604+HumanThe2nd@users.noreply.github.com>
2024-04-02 12:47:16 -0400

    Create dwite12c5p3.c

---
+++ dwite12c5p3.c
@@ -0,0 +1,31 @@
+// DWITE '12 R5 #3 - Triangle Count
+// By: Dan Shan
+// Theory: Same as CCO '14 P1 - Troyangles (cco14p1)
+#include <stdio.h>
+#define min(a,b)((a)<(b)?(a):(b))
+int main() {
+    for(int t=0;t<5;t++){
+    int n,c=0;
+    scanf("%d",&n);
+    int dp[n][n];
+    for(int i=0;i<n;i++){
+        getchar_unlocked();
+        for(int j=0;j<n;j++){
+            char ch = getchar_unlocked();
+            dp[i][j] = ch=='#';
+            c+=dp[i][j];
+        }
+    }
+    for(int i=n-1;i>0;i--){
+        for(int j=1;j<n-1;j++){
+            int t=min(dp[i][j],dp[i][j-1]);
+            t = min(t,dp[i][j+1]);
+            if(dp[i-1][j]){
+                dp[i-1][j]=t+1;
+                c+=t;
+            }
+        }
+    }
+    printf("%d\n",c);
+    }
+}
