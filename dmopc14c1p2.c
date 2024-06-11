f3d949dd7e944254f4ba878b9d4a1bdf5682139a
Dan Shan <96434604+HumanThe2nd@users.noreply.github.com>
2024-06-10 20:08:48 -0400

    Create dmopc14c1p2.c

---
+++ dmopc14c1p2.c
@@ -0,0 +1,9 @@
+// DMOPC '14 Contest 1 P2 - Tiles
+// Author: Dan Shan
+// floor division
+#include <stdio.h>
+int main(int argc, char *argv[]) {
+    int a,b,c;
+    scanf("%d %d %d",&a,&b,&c);
+    printf("%d\n",(a/c)*(b/c));
+}
