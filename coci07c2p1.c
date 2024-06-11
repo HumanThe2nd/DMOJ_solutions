f6cd9ea16ef9bde612d1521b57e9aab99cd0b12c
Dan Shan <96434604+HumanThe2nd@users.noreply.github.com>
2024-06-11 12:32:10 -0400

    Create coci07c2p1.c

---
+++ coci07c2p1.c
@@ -0,0 +1,8 @@
+// COCI '07 Contest 2 #1 Bijele
+// Author: Dan Shan
+#include <stdio.h>
+int main(int argc, char *argv[]) {
+    int a,b,c,d,e,f;
+    scanf("%d %d %d %d %d %d",&a,&b,&c,&d,&e,&f);
+    printf("%d %d %d %d %d %d\n",1-a,1-b,2-c,2-d,2-e,8-f);
+}
