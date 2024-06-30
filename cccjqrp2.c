a5df2e0d917c0bbe4809c7c6d477ce329bf81620
Dan Shan <96434604+HumanThe2nd@users.noreply.github.com>
2024-06-30 10:17:29 -0400

    Create cccjqrp2.c

---
+++ cccjqrp2.c
@@ -0,0 +1,11 @@
+// CCCJQR P2
+// Author: Dan Shan
+// it was a trick question just output n assuming he wins all
+// if you actually check power levels it will fail
+#include <stdio.h>
+int main() {
+    for(int i=0;i<9;i++) getchar_unlocked();
+	int n;
+	scanf("%d %d",&n,&n);
+	printf("%d\n",n);
+}
