64d53deaaa2d96a9dffd2bffd78100c77fbeafcc
Dan Shan <96434604+HumanThe2nd@users.noreply.github.com>
2024-07-13 07:48:02 +0200

    Create ampl2024sp1.c

---
+++ ampl2024sp1.c
@@ -0,0 +1,9 @@
+// Amplitude Hackathon Summer '24 Problem 1 - Is Jeffrey in the Office?
+// Author: Dan Shan
+// First letter is sufficient 
+#include <stdio.h>
+int main() {
+    char c;
+	c=getchar_unlocked();
+    printf("%s\n",c=='S'?"NO":"YES");
+}
