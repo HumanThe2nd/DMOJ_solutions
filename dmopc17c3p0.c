c9a8011070df4acc63943c0f064e70cf7ba4309a
Human <96434604+HumanThe2nd@users.noreply.github.com>
2024-11-22 23:29:25 -0500

    Create dmopc17c3p0.c

---
+++ dmopc17c3p0.c
@@ -0,0 +1,12 @@
+/*
+DMOPC '17 Contest 3 P0 - 2-Satisfiability
+Author: Dan Shan
+Date: 2024-11-22
+half the input to 2 decimal places twice
+*/ 
+#include <stdio.h>
+int main() {
+    double n;
+    scanf("%lf",&n);
+    printf("$%0.2lf $%0.2lf\n",n/2,n/2);
+}
