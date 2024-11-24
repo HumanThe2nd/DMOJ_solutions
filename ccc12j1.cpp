de8f4c17192e71292f4c9004121a0bc30bb29b60
Human <96434604+HumanThe2nd@users.noreply.github.com>
2024-11-24 11:07:31 -0500

    Update ccc12j1.cpp

--- ccc12j1.cpp
+++ ccc12j1.cpp
@@ -1,16 +1,18 @@
-#include <iostream>
-using namespace std;
+/*
+CCC '12 J1 - Speed fines are not fine!
+Author: Dan Shan
+Date: 2024-11-24
+if statments + conditionals
+*/
+#include<stdio.h>
 int main() {
-  int limit, speed,x;
-  cin >> limit >> speed;
-  if(limit > speed)
-    cout << "Congratulations, you are within the speed limit!";
-  else{
-    x = speed - limit;
-    cout << "You are speeding and your fine is $";
-    if(x < 21) cout << 100;
-    else if(x < 30) cout << 270;
-      else cout << 500;
-    cout << ".";
-  }
+    int l,s;
+    scanf("%d %d",&l,&s);
+    if(s<=l) return printf("Congratulations, you are within the speed limit!\n")*0;
+    int x=s-l;
+    printf("You are speeding and your fine is $");
+    if(x>30) printf("500");
+    else if(x>20) printf("270");
+    else printf("100");
+    printf(".\n");
 }
