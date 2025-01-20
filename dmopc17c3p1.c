f35e068cdd3907ca2f4357377488a5acf0c7b878
Human <96434604+HumanThe2nd@users.noreply.github.com>
2025-01-19 22:05:05 -0500

    Create dmopc17c3p1.c

---
+++ dmopc17c3p1.c
@@ -0,0 +1,18 @@
+/*
+DMOPC '17 Contest 3 P1 - An Early Christmas Present
+Author: Dan Shan
+Date: 2025-01-18
+min element
+*/
+#include <stdio.h>
+int main(){
+    int n,m;
+    scanf("%d %d",&n,&m);
+    n--;
+    while(n--){
+        int ai;
+        scanf("%d",&ai);
+        if(ai<m) m=ai;
+    }
+    printf("%d\n",m);
+}
