22151cbd7aa412abd2c4c1d9d958998310d5a9bc
Human <96434604+HumanThe2nd@users.noreply.github.com>
2025-02-09 13:04:17 -0500

    Create nccc7j3.c

---
+++ nccc7j3.c
@@ -0,0 +1,11 @@
+/*
+Mock CCC '19 Contest 2 J3 - Tudor Buys Some Tea
+Author: Dan Shan
+Date: 2025-02-09
+*/
+#include <stdio.h>
+int main() {
+   long long n,k;
+   scanf("%lld %lld",&n,&k);
+   printf("%lld\n",n-n/(k+1));
+}
