9beb8f14b0054cc9e51a3fc4b45f2129291a6664
Human <96434604+HumanThe2nd@users.noreply.github.com>
2025-02-26 12:39:02 -0500

    Update and rename ccc23j2.py to CCC/2023/ccc23j2.py

--- CCC/2023/ccc23j2.py
+++ CCC/2023/ccc23j2.py
@@ -1,3 +1,6 @@
+# CCC '23 J2 - Chili Peppers
+# Author: HumanThe2nd
+# Date: 2023-06-27
 num = int(input())
 values = [1500, 6000, 15500, 40000, 75000, 125000]
 names = ["Poblano", "Mirasol", "Serrano", "Cayenne", "Thai", "Habanero"]
