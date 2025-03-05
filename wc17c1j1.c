74a1cc1e15d6196e2fb416786e4c2692051c7015
Human <96434604+HumanThe2nd@users.noreply.github.com>
2025-03-04 23:06:40 -0500

    Create wc17c1j1.c

---
+++ wc17c1j1.c
@@ -0,0 +1,11 @@
+/*
+WC '17 Contest 1 J1 - Canadian Accent
+Author: Dan Shan
+Date: 2025-03-04
+*/
+#include <stdio.h>
+int main() {
+    char s[11];
+    scanf("%s",s);
+    printf("%s, eh\n",s);
+}
