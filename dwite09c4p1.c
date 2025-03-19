427d0ca724c3c95d8b2beec169ddb1430f96d2a9
Human <96434604+HumanThe2nd@users.noreply.github.com>
2025-03-19 18:36:40 -0400

    Create dwite09c4p1.c

---
+++ dwite09c4p1.c
@@ -0,0 +1,16 @@
+/*
+DWITE '09 R4 #1 - Social Media Overload
+Author: HumanThe2nd
+Date: 2025-03-019
+Round 9000/n
+*/
+#include <stdio.h>
+#include <math.h>
+#pragma GCC optimize("Ofast")
+int main() {
+    for(int i=0;i<5;i++){
+        double n;
+        scanf("%lf",&n);
+        printf("%d\n",(int)round(9000/n));
+    }
+}
