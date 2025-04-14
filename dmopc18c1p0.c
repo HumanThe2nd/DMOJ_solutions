0d6c378291ae91522985e6200ce0fdb4468dce2a
Human <96434604+HumanThe2nd@users.noreply.github.com>
2025-04-14 17:24:51 -0400

    Create dmopc18c1p0.c

---
+++ dmopc18c1p0.c
@@ -0,0 +1,13 @@
+/*
+DMOPC '18 Contest 1 P0 - Sorting
+Author: HumanThe2nd
+Date: 2025-04-14
+2 comparisions
+*/
+#include <stdio.h>
+int main() {
+    int a,b,c;
+    scanf("%d %d %d",&a,&b,&c);
+    if(a>b||b>c) puts("Try again!");
+    else puts("Good job!");
+}
