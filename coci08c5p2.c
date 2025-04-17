90cb95bbb131df95564e96292e9e3ffc66d95eca
Human <96434604+HumanThe2nd@users.noreply.github.com>
2025-04-16 20:06:25 -0400

    Create coci08c5p2.c

---
+++ coci08c5p2.c
@@ -0,0 +1,24 @@
+/*
+COCI '08 Contest 5 #2 Jabuka
+Author: HumanThe2nd
+Date: 2025-04-16
+store all factors of R
+check whether each is disvisible by G
+Time complexity: O( sqrt(R) )
+*/
+#include <stdio.h>
+int main() {
+    int r,g;
+    scanf("%d %d",&r,&g);
+    int q[100001],l=0;
+    for(int i=1;i*i<=r;i++){
+        if(r%i) continue; // not divisible
+        q[l++]=i;
+        if(r/i!=i) q[l++]=r/i;
+    }
+    for(int i=0;i<l;i++){
+        int ai=q[i];
+        if(g%ai) continue; // not divisible
+        printf("%d %d %d\n",ai,r/ai,g/ai);
+    }
+}
