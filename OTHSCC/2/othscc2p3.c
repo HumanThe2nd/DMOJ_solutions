bde72ffe6681215acb5af18a76e172b87b841c61
Human <96434604+HumanThe2nd@users.noreply.github.com>
2025-05-14 16:38:09 -0400

    Create othscc2p3.c

---
+++ OTHSCC/2/othscc2p3.c
@@ -0,0 +1,12 @@
+/*
+OTHS Coding Competition 2 P3 - Tally Clicker
+Author: Dan Shan
+Date: 2024-07-30
+Observation: Division + Modulus
+*/
+#include <stdio.h>
+int main() {
+    short n;
+	scanf("%hd",&n);
+	printf("%hd\n",n/1111+n%1111);
+}
