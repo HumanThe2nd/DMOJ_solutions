932292623d25ad8e45c0c75021c59c74eefde092
Human <96434604+HumanThe2nd@users.noreply.github.com>
2025-05-27 10:46:45 -0400

    Create mccc5j1.c

---
+++ mccc5j1.c
@@ -0,0 +1,13 @@
+/*
+Mock CCC '23 Contest 1 J1 - Final Velocity
+Author: Dan Shan
+Date: 2025-05-27
+Formula: Vf = u + 3t
+*/
+#include <stdio.h>
+#pragma GCC optimize ("Ofast")
+int main(){
+    int u,t;
+    scanf("%d %d",&u,&t);
+    printf("%d\n",u+3*t);
+}
