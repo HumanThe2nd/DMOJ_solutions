8885749037f3689335c55ff86e5185f883eee9be
Human <96434604+HumanThe2nd@users.noreply.github.com>
2025-06-14 18:22:27 -0400

    Update ccc01s2.cpp

--- CCC/2001/ccc01s2.cpp
+++ CCC/2001/ccc01s2.cpp
@@ -1,5 +1,57 @@
-// CCC '01 S2 - Spirals
-// By: Dan Shan
+/*
+CCC '01 S2 - Spirals
+Author: Dan Shan
+Date: 2025-06-14
+2d deque -> simple & efficient updates
+Pattern modulo 4: left(0),down(1), right(2), up(3)
+lengths: 1,1,2,2,3,3,4,4, etc
+*/
+
+// Newer Solution
+#include <bits/stdc++.h>
+#pragma GCC optimize("Ofast")
+using namespace std;
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int x,y,p=1;
+    cin >> x >> y;
+    deque<deque<int>> dq={{x++}};
+    while(x<=y){
+        int si=dq.size();
+        if(p%4==0){ // left
+            for(int i=0;i<(p>>1)&&x<=y;++i){
+                dq.front().emplace_front(x++);
+            }
+        }
+        else if(p%4==1){ // down
+            for(int i=1;i<=(p>>1)&&x<=y;++i){
+                dq[i].emplace_front(x++);
+            }
+            if(x<=y) dq.emplace_back(deque<int>{x++});
+        }
+        else if(p%4==2){ // right
+            for(int i=0;i<(p>>1)&&x<=y;++i){
+                dq.back().emplace_back(x++);
+            }
+        }
+        else{ // up
+            for(int i=2;i<=dq.size()&&x<=y;++i){
+                dq[dq.size()-i].emplace_back(x++);
+            }
+            if(x<=y) dq.emplace_front(deque<int>{x++});
+        } ++p; // next
+    }
+    for(auto x: dq){
+        for(auto y: x){
+            cout << y << " ";
+        }
+        cout << "\n";
+    }
+}
+
+// Old Solution
+// Date: 2023-09-10
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
