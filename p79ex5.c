30842c6d9a1685f3fe1ad1514c0e220b7a564f13
Dan Shan <96434604+HumanThe2nd@users.noreply.github.com>
2025-07-11 19:19:10 -0400

    Create p79ex5.c

---
+++ p79ex5.c
@@ -0,0 +1,13 @@
+/*
+BlueBook - Multiple
+Author: Dan Shan
+Date: 2025-07-11
+Modular arithmetic
+*/
+#include <stdio.h>
+#pragma GCC optimize ("Ofast")
+int main(){
+    int a,b;
+    scanf("%d %d",&a,&b);
+    puts(a%b?"no":"yes");
+}
