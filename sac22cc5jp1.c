b3734ea61ffc8b03111eb3d81eaab7f46fa299fb
Dan Shan <96434604+HumanThe2nd@users.noreply.github.com>
2025-09-03 23:53:10 -0400

    Create sac22cc5jp1.c

---
+++ sac22cc5jp1.c
@@ -0,0 +1,13 @@
+/*
+SAC '22 Code Challenge 5 Junior P1 - Monkey, Type!
+Author: Dan Shan
+Date: 2025-09-03
+print last input - first input
+*/
+#include <stdio.h>
+
+int main() {
+    int a,b;
+    scanf("%d %d %d",&a,&b,&b);
+    printf("%d\n",b-a);
+}
