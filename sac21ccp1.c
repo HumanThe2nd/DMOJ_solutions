e0d0945e44843f04dafd7dd447bcabbb3022fa93
Dan Shan <96434604+HumanThe2nd@users.noreply.github.com>
2025-09-29 19:57:03 -0400

    Create sac21ccp1.c

---
+++ sac21ccp1.c
@@ -0,0 +1,14 @@
+/*
+SAC '21 Code Challenge P1 - Shrinkage
+Author: Dan Shan
+Date: 2025-09-29
+
+Divide input by 40 and return
+*/
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    printf("%d\n", n / 40);
+}
